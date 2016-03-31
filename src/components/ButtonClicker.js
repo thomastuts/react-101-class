04de94bbe1a3cef6dd4cde47a767e0acde7b4006
Thomas Tuts <thomas@thomastuts.com>
2016-03-31 12:15:33 +0200

    Add briefing for Button Clicker exercise

---
+++ src/components/ButtonClicker.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+/**
+ * This is the Button Clicker exercise.
+ * Codepen link: http://codepen.io/thomastuts/pen/zqwJVV?editors=1010
+ *
+ * Briefing
+ * ----
+ * This component has a button (the one that is already in the `render()` method down below). When
+ * the button is clicked, it should call a method that is defined on the class (you are free to choose
+ * the name of that method). When the method is called, it should log out a simple message
+ * using `console.log()`.
+ *
+ * Hints
+ * ----
+ * Keep in mind that the `render()` method is also a method defined on the class. This means that
+ * the method you're going to write to log out the message is constructed in the same way.
+ *
+ * You pass your event handlers to an element as a camel-cased prop. For a full list of events, see
+ * https://facebook.github.io/react/docs/events.html#supported-events.
+ */
+
+export default class ButtonClicker extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Click me!</button>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Code here!
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ButtonClicker from './components/ButtonClicker';
+
+ReactDOM.render(
+  <ButtonClicker />,
+  document.getElementById('root')
+);
