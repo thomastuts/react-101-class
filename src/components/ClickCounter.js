57ec34c8b8f1292324f66c143fe21f3890425c0b
Thomas Tuts <thomas@thomastuts.com>
2016-03-31 13:19:30 +0200

    Add solution for Click Counter exercise

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -14,11 +14,25 @@ import React from 'react';
  */
 
 export default class ButtonClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleButtonClick() {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    });
+  }
+
   render() {
     return (
       <div>
-        <button>Click me!</button>
-        <p>I have been clicked 0 times.</p>
+        <button onClick={this.handleButtonClick.bind(this)}>Click me!</button>
+        <p>I have been clicked {this.state.timesClicked} times.</p>
       </div>
     );
   }
