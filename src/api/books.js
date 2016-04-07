7e8d303b2e6e234169803327d9a178f077d15302
Thomas Tuts <thomas@thomastuts.com>
2016-04-07 23:00:46 +0200

    Implement a mock API that returns async data

---
+++ src/api/books.js
@@ -0,0 +1,20 @@
+export function getBookData(bookId, cb) {
+  console.log(bookId);
+  setTimeout(function () {
+    if (bookId === 'harry-potter') {
+      cb({
+          title: 'Harry Potter and The Goblet of Fire',
+          image: 'http://placekitten.com/200/201',
+          description: 'Harry Potter goes on a crazy adventure, wow!'
+        }
+      )
+    } else {
+      cb({
+          title: 'Fifty Shades of Grey',
+          image: 'http://placekitten.com/200/201',
+          description: 'Some kind of weird book a lot of people seem to be into.'
+        }
+      )
+    }
+  }, 3000);
+}
--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
-const BookDetail = (props) => {
-  return (
-    <div>
-      Book details for: {props.routeParams.detail}
-    </div>
-  )
-};
+import {getBookData} from '../api/books.js';
+
+export default class BookDetail extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      bookInfo: null
+    };
+  }
+
+  componentDidMount() {
+    getBookData(this.props.routeParams.detail, (bookInfo) => {
+      console.log('oh no');
+      this.setState({
+        bookInfo
+      });
+    });
+  }
+
+  render() {
+    if (!this.state.bookInfo) {
+      return (
+        <p>Loading...</p>
+      );
+    } else {
+      return (
+        <div>
+          <h1>{this.state.bookInfo.title}</h1>
+          <img src={this.state.bookInfo.image} />
+          <p>{this.state.bookInfo.description}</p>
+        </div>
+      );
+    }
+  }
+}
 
 BookDetail.propTypes = {};
-
-export default BookDetail;
