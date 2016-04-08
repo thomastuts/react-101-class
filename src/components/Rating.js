6c6861ee28cf08c57eaaf1a50900d0388a2ae553
Thomas Tuts <thomas@thomastuts.com>
2016-04-08 14:07:57 +0200

    Pass in undefined when binding onRatingClick because that's what it is

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Rating = ({rating, onRatingClick}) => (
   <ul>
-    <li onClick={onRatingClick.bind(this, 1)} className={(rating >= 1) ? 'rating-active' : null}>X</li>
-    <li onClick={onRatingClick.bind(this, 2)} className={(rating >= 2) ? 'rating-active' : null}>X</li>
-    <li onClick={onRatingClick.bind(this, 3)} className={(rating >= 3) ? 'rating-active' : null}>X</li>
-    <li onClick={onRatingClick.bind(this, 4)} className={(rating >= 4) ? 'rating-active' : null}>X</li>
-    <li onClick={onRatingClick.bind(this, 5)} className={(rating >= 5) ? 'rating-active' : null}>X</li>
+    <li onClick={onRatingClick.bind(undefined, 1)} className={(rating >= 1) ? 'rating-active' : null}>X</li>
+    <li onClick={onRatingClick.bind(undefined, 2)} className={(rating >= 2) ? 'rating-active' : null}>X</li>
+    <li onClick={onRatingClick.bind(undefined, 3)} className={(rating >= 3) ? 'rating-active' : null}>X</li>
+    <li onClick={onRatingClick.bind(undefined, 4)} className={(rating >= 4) ? 'rating-active' : null}>X</li>
+    <li onClick={onRatingClick.bind(undefined, 5)} className={(rating >= 5) ? 'rating-active' : null}>X</li>
   </ul>
 );
 
