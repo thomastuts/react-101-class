502e72579d324fec54524a191b4705586f8eb263
Thomas Tuts <thomas@thomastuts.com>
2016-04-14 19:30:16 +0200

    Add test briefings and hints

--- test/components/ClickCounter.js
+++ test/components/ClickCounter.js
@@ -3,15 +3,19 @@ import { shallow } from 'enzyme';
 
 import ClickCounter from '../../src/components/ClickCounter';
 
+/**
+ * Add test logic for the following two tests to verify that the component works like it should.
+ * 
+ * Hint: the component contains a button (that can be used to simulate a click), as well as a span
+ * with a `.timer-clicked-count` class that contains just the amount of times the button has been
+ * clicked. Take a look at `.find()` and `.simulate()` in Enzyme.
+ */
 describe('<ClickCounter />', () => {
   it('should render the initial state with 0 times clicked', () => {
     const wrapper = shallow(<ClickCounter />);
-    wrapper.find('.timer-clicked-count').text().should.equal('0');
   });
 
   it('should update the counter when the button is clicked', () => {
     const wrapper = shallow(<ClickCounter />);
-    wrapper.find('button').simulate('click');
-    wrapper.find('.timer-clicked-count').text().should.equal('1');
   });
 });
--- test/components/GreetingCard.js
+++ test/components/GreetingCard.js
@@ -3,19 +3,22 @@ import { shallow } from 'enzyme';
 
 import GreetingCard from '../../src/components/GreetingCard';
 
+/**
+ * Add test logic for the following three tests to verify that the component works like it should.
+ *
+ * Hint: Enzyme exposes a `.text()` method on the rendered component that returns its entire text
+ * content. Maybe you could use that to see if the text includes the strings we care about?
+ */
 describe('<GreetingCard />', () => {
   it('should render the name being passed in', () => {
     const wrapper = shallow(<GreetingCard name={'Thomas'} />);
-    wrapper.text().includes('Thomas').should.be.true();
   });
 
   it('should render in the language being passed in', () => {
     const wrapper = shallow(<GreetingCard name={'Thomas'} language={'NL'} />);
-    wrapper.text().includes('mijn naam is').should.be.true();
   });
 
   it('should render in English when there is no language specified', () => {
     const wrapper = shallow(<GreetingCard name={'Thomas'} />);
-    wrapper.text().includes('my name is').should.be.true();
   });
 });
